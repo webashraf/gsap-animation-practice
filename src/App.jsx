5f6f46936584f7d378977a9c319aed76738f913a
webashraf <aliashraftamim@gmail.com>
2024-02-26 17:38:46 +0600

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Shared/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div className='bg-black'>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
